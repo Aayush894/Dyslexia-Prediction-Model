dfd4bac1d2324e8033ef0d8d8088f830588ac54d
Aayush Kumar Shrivastava <aayushs894@gmail.com>
2024-02-27 23:22:53 +0530

    Add avatar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,52 +1,69 @@
-import { Link, useNavigate } from 'react-router-dom'
-import ContactSupportIcon from '@mui/icons-material/ContactSupport';
+import { Link, useNavigate } from "react-router-dom";
+import ContactSupportIcon from "@mui/icons-material/ContactSupport";
 
 export default function NavBar() {
-
-  
-  const navigate = useNavigate() ;
+  const navigate = useNavigate();
 
   const handleLogout = () => {
-    localStorage.removeItem("authToken"); 
+    localStorage.removeItem("authToken");
     navigate("/login");
-  }
-  
+  };
+
   return (
     <>
       <nav className="navbar navbar-expand-sm bg-success navbar-dark fixed-top">
         <div className="container-fluid">
+  <Link className="navbar-brand" to="/profile">
+    <img
+      src="./assets/avatar_icon.png"
+      alt="Avatar Logo"
+      style={{width: 40, height: 40, borderRadius: '50%'}}
+    />
+  </Link>
+
           <ul className="navbar-nav me-auto mb-2">
             <li className="nav-item">
-              <Link className="nav-link fw-bold text-white" to="/">Home</Link>
+              <Link className="nav-link fw-bold text-white" to="/">
+                Home
+              </Link>
             </li>
 
-            {(localStorage.getItem("authToken")) ?
+            {localStorage.getItem("authToken") ? (
               <li className="nav-item">
-              <Link className="nav-link fw-bold text-white" to="/testrecord"> Records </Link>
+                <Link className="nav-link fw-bold text-white" to="/testrecord">
+                  {" "}
+                  Records{" "}
+                </Link>
               </li>
-              : ""
-            }
-
+            ) : (
+              ""
+            )}
           </ul>
-          {(!localStorage.getItem("authToken")) ?
+          {!localStorage.getItem("authToken") ? (
             <div className="d-flex">
-            <Link className="btn bg-white text-success mx-1" to="/login">Login</Link>
-            <Link className="btn bg-white text-success mx-1" to="/signup">Signup</Link>
+              <Link className="btn bg-white text-success mx-1" to="/login">
+                Login
+              </Link>
+              <Link className="btn bg-white text-success mx-1" to="/signup">
+                Signup
+              </Link>
             </div>
-            : 
+          ) : (
             <div className="d-flex">
               <Link className="nav-link fw-bold text-white mx-1" to="/contact">
-               <ContactSupportIcon
-               fontSize = "medium" />
+                <ContactSupportIcon fontSize="medium" />
               </Link>
 
-              <div className='btn bg-white text-danger mx-1' onClick={handleLogout}>
+              <div
+                className="btn bg-white text-danger mx-1"
+                onClick={handleLogout}
+              >
                 Logout
               </div>
             </div>
-          }
+          )}
         </div>
       </nav>
     </>
-  )
+  );
 }
