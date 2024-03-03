2f56065d181dab5019919b11070aea3232b3f98a
Aayush Kumar Shrivastava <aayushs894@gmail.com>
2024-03-03 13:19:34 +0530

    resolve small typo

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -88,7 +88,7 @@ const Navbar = () => {
                         Test
                       </Link>
                       <Link
-                        to="/record "
+                        to="/testrecord "
                         className="w-full px-4 py-2 -ml-4 text-gray-500 rounded-md dark:text-gray-300 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 dark:focus:bg-gray-800 focus:outline-none"
                       >
                         History
