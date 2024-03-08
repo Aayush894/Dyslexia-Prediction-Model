8ca8dca923b5d744f1c1dc85d994776a1be78d8b
Aayush Kumar Shrivastava <aayushs894@gmail.com>
2024-03-08 22:28:05 +0530

    Just format

--- frontend/src/pages/Home/Banner.jsx
+++ frontend/src/pages/Home/Banner.jsx
@@ -1,64 +1,60 @@
-import React from 'react'
-import { Carousel } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-// import img1 from "../Images/slider-01.jpg"
-// import img2 from "../Images/slider-02.jpg"
-// import img3 from "../Images/slider-03.jpg"
-
-
+import { Carousel } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Banner() {
   return (
     <div>
-      <div className='container-fluid' >
-
-
+      <div className="container-fluid">
         <div className="row">
           <div className="col-12">
             <Carousel>
-
               <Carousel.Item>
                 <img
                   className="d-block w-100"
-                  src="./public/assets/disgraphia1.png"
+                  src="/assets/disgraphia1.png"
                   alt="First slide"
-
                 />
                 <Carousel.Caption>
-                  <h3 class='text-dark'>First slide label</h3>
-                  <p class='text-dark'>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                  <h3 className="text-dark">First slide label</h3>
+                  <p className="text-dark">
+                    Nulla vitae elit libero, a pharetra augue mollis interdum.
+                  </p>
                 </Carousel.Caption>
               </Carousel.Item>
 
               <Carousel.Item>
                 <img
                   className="d-block w-100"
-                  src="./public/assets/disgraphia3.png"
+                  src="/assets/disgraphia3.png"
                   alt="Second slide"
                 />
 
                 <Carousel.Caption>
-                  <h3 class='text-dark'>Second slide label</h3>
-                  <p class='text-dark'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                  <h3 className="text-dark">Second slide label</h3>
+                  <p className="text-dark">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  </p>
                 </Carousel.Caption>
               </Carousel.Item>
 
               <Carousel.Item>
                 <img
                   className="d-block w-100"
-                  src="./public/assets/disgraphia2.png"
+                  src="assets/disgraphia2.png"
                   alt="Third slide"
                 />
                 <Carousel.Caption>
-                  <h3 class='text-dark'>Third slide label</h3>
-                  <p class='text-dark'>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                  <h3 className="text-dark">Third slide label</h3>
+                  <p className="text-dark">
+                    Praesent commodo cursus magna, vel scelerisque nisl
+                    consectetur.
+                  </p>
                 </Carousel.Caption>
               </Carousel.Item>
-
             </Carousel>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
