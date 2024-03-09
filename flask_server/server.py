54715d8713ae5cefedcbdbd628528aef4c1613e1
Aayush Kumar Shrivastava <aayushs894@gmail.com>
2024-03-09 23:10:25 +0530

    integrate flask sever

--- flask_server/server.py
+++ flask_server/server.py
@@ -1,188 +1,72 @@
-from flask import Flask, jsonify, request, render_template
-import speech_recognition as sr
+from flask import Flask, request, render_template, Response
+from flask_cors import CORS
 from gtts import gTTS
 import os
 import random
 import csv
-import time
-from flask import redirect, url_for
-from flask import Response
-from flask import session
-from pathlib import Path
-from textblob import TextBlob
-# import streamlit as st
-from PIL import Image
-import os
-import language_tool_python
-import requests
-import pandas as pd
-import random
-import speech_recognition as sr
-import pyttsx3
-import time
-import eng_to_ipa as ipa
-import requests
-from abydos.phonetic import Soundex, Metaphone, Caverphone, NYSIIS 
+
+from testdir.test import levenshtein, get_feature_array, score
+from flask import jsonify
 
 app = Flask(__name__)
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+CORS(app, origins="http://localhost:3000", supports_credentials=True, methods=["GET", "POST", "PUT", "PATCH", "DELETE"], allow_headers=["Content-Type", "Authorization"], expose_headers=["Content-Length", "Authorization"])
 
-# algorithm to match the strings
-def levenshtein(s1, s2):
-  if len(s1) < len(s2):
-    return levenshtein(s2, s1)
+# Computer will speak almost 10 words
+spoken_words = []
 
-  # len(s1) >= len(s2)
-  if len(s2) == 0:
-    return len(s1)
+@app.route('/api/fetchWords', methods=['POST'])
+def fetch_words():
+    # Load the elementary vocabulary from CSV
+    global spoken_words
+    spoken_words.clear()
+    vocabulary = load_elementary_vocabulary()
 
-  previous_row = range(len(s2) + 1)
-  for i, c1 in enumerate(s1):
-    current_row = [i + 1]
-    for j, c2 in enumerate(s2):
-      # j+1 instead of j since previous_row and current_row are one character longer
-        insertions = previous_row[j + 1] + 1
-        deletions = current_row[j] + 1       # than s2
-        substitutions = previous_row[j] + (c1 != c2)
-        current_row.append(min(insertions, deletions, substitutions))
-    previous_row = current_row
+    # Select and return 10 random words
+    random_words = random.sample(vocabulary, 10)
 
-  return previous_row[-1]
+    # Store the selected random words in the spoken_words array
+    spoken_words.extend(random_words)
 
-# Calculate the dependent fields of the methods  
-def spelling_accuracy(extracted_text):
-    spell_corrected = TextBlob(extracted_text).correct()
-    return ((len(extracted_text) - (levenshtein(extracted_text, spell_corrected)))/(len(extracted_text)+1))*100
+    return jsonify(random_words)
 
-# ***************************************************
-my_tool = language_tool_python.LanguageTool('en-US')
+def load_elementary_vocabulary():
+    vocabulary = []
+    resources_folder = os.path.join(os.getcwd(), 'resources')
+    csv_file_path = os.path.join(resources_folder, 'elementary_voc.csv')
 
-def gramatical_accuracy(extracted_text):
-  spell_corrected = TextBlob(extracted_text).correct()
-  correct_text = my_tool.correct(spell_corrected)
-  extracted_text_set = set(spell_corrected.split(" "))
-  correct_text_set = set(correct_text.split(" "))
-  n = max(len(extracted_text_set - correct_text_set),
-          len(correct_text_set - extracted_text_set))
-  return ((len(spell_corrected) - n)/(len(spell_corrected)+1))*100
+    with open(csv_file_path, 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            vocabulary.extend(row)
+    return vocabulary
 
-# ****************************************************
+# Submit words from form
+@app.route('/api/submitWords', methods=['POST'])
+def submit_words():
+    submitted_words = [request.form.get(f'word{i}', '') for i in range(1, 11)]
+    score = levenshtein(spoken_words, submitted_words)
+    return Response({score})
 
-# text correction API authentication
-api_key_textcorrection = "eaeb9fb5a72f4e529111856dfabd43aa"
-endpoint_textcorrection = "https://api.bing.microsoft.com/"
+# # Submit text from form
+# @app.route('/submit_text', methods=['POST'])
+# def submit_text():
+#     if request.method == 'POST':
+#         user_text = request.form['user_text']
+#         print("User submitted text:", user_text)
 
-def percentage_of_corrections(extracted_text):
-  data = {'text': extracted_text}
-  params = {
-    'mkt': 'en-us',
-    'mode': 'proof'
-  }
-  headers = {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Ocp-Apim-Subscription-Key': api_key_textcorrection,
-  }
-  response = requests.post(endpoint_textcorrection, headers=headers, params=params, data=data)
-  json_response = response.json()
-  flagged_tokens_count = len(json_response.get('flaggedTokens', []))
-  extracted_word_count = len(extracted_text.split(" "))
-  if extracted_word_count > 0:
-    percentage_corrected = (flagged_tokens_count / extracted_word_count) * 100
-  else:
-    percentage_corrected = 0
-  return percentage_corrected
+#         # Use the user_text as input for get_feature_array
+#         feature_array = get_feature_array(user_text)
+#         result = score(feature_array)
 
-# ****************************************************
+#         if result[0] == 1:
+#             word = "From the tests on this handwriting sample, there is a very slim chance that this person is suffering from dyslexia or dysgraphia"
+#             print("From the tests on this handwriting sample, there is a very slim chance that this person is suffering from dyslexia or dysgraphia")
+#         else:
+#             word = "From the tests on this handwriting sample, there is a very high chance that this person is suffering from dyslexia or dysgraphia"
+#             print("From the tests on this handwriting sample, there is a very high chance that this person is suffering from dyslexia or dysgraphia")
 
-def percentage_of_phonetic_accuraccy(extracted_text: str):
-  soundex = Soundex()
-  metaphone = Metaphone()
-  caverphone = Caverphone()
-  nysiis = NYSIIS()
-  spell_corrected = TextBlob(extracted_text).correct()
+#     # Render index.html and pass the prediction word
+#     return render_template('index.html', prediction=word)
 
-  extracted_text_list = extracted_text.split(" ")
-  extracted_phonetics_soundex = [soundex.encode(string) for string in extracted_text_list]
-  extracted_phonetics_metaphone = [metaphone.encode(string) for string in extracted_text_list]
-  extracted_phonetics_caverphone = [caverphone.encode(string) for string in extracted_text_list]
-  extracted_phonetics_nysiis = [nysiis.encode(string) for string in extracted_text_list]
-
-  extracted_soundex_string = " ".join(extracted_phonetics_soundex)
-  extracted_metaphone_string = " ".join(extracted_phonetics_metaphone)
-  extracted_caverphone_string = " ".join(extracted_phonetics_caverphone)
-  extracted_nysiis_string = " ".join(extracted_phonetics_nysiis)
-
-  spell_corrected_list = spell_corrected.split(" ")
-  spell_corrected_phonetics_soundex = [soundex.encode(string) for string in spell_corrected_list]
-  spell_corrected_phonetics_metaphone = [metaphone.encode(string) for string in spell_corrected_list]
-  spell_corrected_phonetics_caverphone = [caverphone.encode(string) for string in spell_corrected_list]
-  spell_corrected_phonetics_nysiis = [nysiis.encode(string) for string in spell_corrected_list]
-
-  spell_corrected_soundex_string = " ".join(spell_corrected_phonetics_soundex)
-  spell_corrected_metaphone_string = " ".join(spell_corrected_phonetics_metaphone)
-  spell_corrected_caverphone_string = " ".join(spell_corrected_phonetics_caverphone)
-  spell_corrected_nysiis_string = " ".join(spell_corrected_phonetics_nysiis)
-
-  soundex_score = (len(extracted_soundex_string)-(levenshtein(extracted_soundex_string,spell_corrected_soundex_string)))/(len(extracted_soundex_string)+1)
-  # print(spell_corrected_soundex_string)
-  # print(extracted_soundex_string)
-  # print(soundex_score)
-  metaphone_score = (len(extracted_metaphone_string)-(levenshtein(extracted_metaphone_string,spell_corrected_metaphone_string)))/(len(extracted_metaphone_string)+1)
-  # print(metaphone_score)
-  caverphone_score = (len(extracted_caverphone_string)-(levenshtein(extracted_caverphone_string,spell_corrected_caverphone_string)))/(len(extracted_caverphone_string)+1)
-  # print(caverphone_score)
-  nysiis_score = (len(extracted_nysiis_string)-(levenshtein(extracted_nysiis_string,spell_corrected_nysiis_string)))/(len(extracted_nysiis_string)+1)
-  # print(nysiis_score)
-  return ((0.5*caverphone_score + 0.2*soundex_score + 0.2*metaphone_score + 0.1 * nysiis_score))*100
-
-def calculate_score(extracted_phonetics, spell_corrected_phonetics):
-    total_distance = sum(levenshtein(extracted, corrected) for extracted, corrected in zip(extracted_phonetics, spell_corrected_phonetics))
-    return (1 - total_distance / len(extracted_phonetics)) if extracted_phonetics else 0
-
-# def get_feature_array(path: str):
-def get_feature_array(text):
-  # path is the path of image, but i am using text.******************************IMAGE****************
-  feature_array = []
-  # extracted_text = image_to_text(path)
-  # *****************************************************************************************
-  extracted_text = text
-  # *****************************************************************************************
-  feature_array.append(spelling_accuracy(extracted_text))
-  feature_array.append(gramatical_accuracy(extracted_text))
-  feature_array.append(percentage_of_corrections(extracted_text))
-  feature_array.append(percentage_of_phonetic_accuraccy(extracted_text))
-  return feature_array
-
-def generate_csv(folder: str, label: int, csv_name: str):
-  arr = []
-  for image in os.listdir(folder):
-    path = os.path.join(folder, image)
-    feature_array = get_feature_array(path)
-    feature_array.append(label)
-    # print(feature_array)
-    arr.append(feature_array)
-    print(feature_array)
-  print(arr)
-  pd.DataFrame(arr, columns=["spelling_accuracy", "gramatical_accuracy", " percentage_of_corrections",
-                "percentage_of_phonetic_accuraccy", "presence_of_dyslexia"]).to_csv("test1.csv")
-
-def score(input):
-  if input[0] <= 96.40350723266602:
-    var0 = [0.0, 1.0]
-  else:
-    if input[1] <= 99.1046028137207:
-        var0 = [0.0, 1.0]
-    else:
-      if input[2] <= 2.408450722694397:
-        if input[2] <= 1.7936508059501648:
-            var0 = [1.0, 0.0]
-        else:
-            var0 = [0.0, 1.0]
-      else:
-            var0 = [1.0, 0.0]
-  return var0
-
-
-
-if __name__ == "__main__": 
-    app.run(debug=True, port=8000, host='0.0.0.0') 
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
--- frontend/src/pages/Prediction/TextPrediction.jsx
+++ frontend/src/pages/Prediction/TextPrediction.jsx
@@ -1,14 +1,103 @@
-import Footer from '../../components/Footer'
+import NavBar from "./../../components/NavBar/NavBar.jsx";
+import Footer from "../../components/Footer";
+import { useState, useEffect } from "react";
 
 function TextPrediction() {
+  const [words, setWords] = useState(""); 
+  const [spoken_text, setSpokenText] = useState([]);
+  const [score, setScore] = useState(0); 
+
+  useEffect(() => {
+    fetchSpokenText();
+  }, []); 
+
+  const fetchSpokenText = () => {
+    fetch("http://127.0.0.1:8000/api/fetchWords")
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch spoken text");
+        }
+        console.log(response) ;
+        return response.json();
+      })
+      .then(data => {
+        // Set fetched spoken text in state
+        setSpokenText(data);
+      })
+      .catch(error => {
+        console.error("Error fetching spoken text:", error);
+      });
+  };
+
+  console.log(spoken_text) ; 
+
+  const handleSpeak = () => {
+    // Join the spoken text array into a single string
+    const spokenText = spoken_text.join(' ');
+    // Create a new SpeechSynthesisUtterance instance with the spoken text
+    const utterance = new SpeechSynthesisUtterance(spokenText);
+    // Speak the text
+    window.speechSynthesis.speak(utterance);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let inputWords = ""; 
+    const form = event.target;
+    const formData = new FormData(form);
+  
+    for (let i = 1; i <= 10; i++) {
+      const word = formData.get(`word${i}`);
+      inputWords += word + " ";
+      form.elements[`word${i}`].value = "";
+    }
+    setWords(inputWords.trim());
+  };
+
   return (
     <>
-      <div>TextPrediction</div>
-      <div>
-        <Footer />
+      <NavBar />
+      <div className="flex flex-col h-screen mb-20">
+        <div className="flex-grow">
+          <div className="container mx-auto p-4">
+            <h1 className="text-3xl font-bold mb-4">Type your words here, and let Google Assistant speak them aloud.</h1>
+            <form onSubmit={handleSpeak}>
+              <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">Make Google Assistant speak</button>
+            </form>
+
+            <h2 className="text-xl font-bold mt-8">Enter 10 words to be spoken</h2>
+            <form onSubmit={handleSubmit} className="mt-4">
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {[...Array(10)].map((_, index) => (
+                  <div key={index}>
+                    <label htmlFor={`word${index + 1}`} className="block mb-2 font-bold">Word {index + 1}:</label>
+                    <input type="text" id={`word${index + 1}`} name={`word${index + 1}`} className="border border-gray-300 rounded px-4 py-2 w-full bg-white" />
+                  </div>
+                ))}
+              </div>
+              <button type="submit" className="mt-4 bg-green-500 text-white px-4 py-2 rounded">Submit</button>
+            </form>
+
+            <div className="info-section mt-8">
+              <h2 className="text-xl font-bold">What you wrote:</h2>
+              {words ? <p>{words}</p> : <p>No words submitted yet.</p>}
+            </div>
+
+            <div className="info-section">
+              <h2 className="text-xl font-bold">What Google Assistant said:</h2>
+              {spoken_text.length > 0 ? <p>{spoken_text.join(' ')}</p> : <p>No text spoken yet.</p>}
+            </div>
+
+            <div className="info-section">
+              <h2 className="text-xl font-bold">Score (minimum is better):</h2>
+              {score ? <p>{score}</p> : <p>No text spoken yet.</p>}
+            </div>
+          </div>
+        </div>
       </div>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default TextPrediction
+export default TextPrediction;
