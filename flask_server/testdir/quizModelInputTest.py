7ffdb814031ceb95844d3beca062e74926a3f800
Abhishek Verma <vermaabhimanyu593@gmail.com>
2024-04-27 19:10:25 +0530

    input test is now correct

--- flask_server/testdir/quizModelInputTest.py
+++ flask_server/testdir/quizModelInputTest.py
@@ -1,13 +1,26 @@
 from quizModel import model
-# import DyslexiaML_final
 import numpy as np
 
-#Giving user description of model
-print("Our model uses RandomForestClassifier with GridSearchCV to predict values from the given data.")
-print("(Our model makes precitions with an error rate of 5.80%.)\n")
+def get_result(lang_vocab, memory, speed, visual, audio, survey):
+  # Create a 2D numpy array with the values input by the user.
+  array = np.array([[lang_vocab, memory, speed, visual, audio, survey]])
+  # The output given by model is converted into an int and stored in label.
+  label = int(model.predict(array)[0])  # Ensure to extract a single element from the result
+  # Giving final output to user depending upon the model prediction.
+  if label == 0:
+    output = "There is a high chance of the applicant to have dyslexia."
+  elif label == 1:
+    output = "There is a moderate chance of the applicant to have dyslexia."
+  else:
+    output = "There is a low chance of the applicant to have dyslexia."
+  return output
 
-#Getting input from user
-name = input("Enter name of appllicant: ")
+# Giving user description of model
+print("Our model uses RandomForestClassifier with GridSearchCV to predict values from the given data.")
+print("(Our model makes predictions with an error rate of 5.80%.)\n")
+
+# Getting input from user
+name = input("Enter name of applicant: ")
 print("\nThe scores of all the tests in quiz as well as survey need to be entered.")
 print("All the values lie in the range 0 to 1.\n")
 lang_vocab = float(input("Enter the score of Language Vocab test: "))
@@ -17,22 +30,6 @@ visual = float(input("Enter the score of Visual Discrimination test: "))
 audio = float(input("Enter the score of Audio Discrimination test: "))
 survey = float(input("Enter the score obtained from Survey: "))
 
-#Creating a 2D array from the given scores.
-#Input taken by our model is a 2D array.
-#get_result function can be run to get result from the survey.
-def get_result(lang_vocab, memory, speed, visual, audio, survey):
-    #2D numpy array created with the values input by the user.
-    array = np.array([[lang_vocab, memory, speed, visual, audio, survey]])
-    #The output given by model is converted into an int and stored in label.
-    label = int(model.predict(array))
-    #Giving final output to user depending upon the model prediction.
-    if(label == 0):
-        output = "There is a high chance of the applicant to have dyslexia."
-    elif(label == 1):
-        output = "There is a moderate chance of the applicant to have dyslexia."
-    else:
-        output = "There is a low chance of the applicant to have dyslexia."
-    return output
-
-get_result(lang_vocab, memory, speed, visual, audio, survey)
-
+# Call get_result function to get the result
+result = get_result(lang_vocab, memory, speed, visual, audio, survey)
+print(result)
