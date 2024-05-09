5a808cbeb1e11778d8d65e625d922c37e54951d3
Aayush Kumar Shrivastava <aayushs894@gmail.com>
2024-05-09 22:17:14 +0530

    Add Rate Limiter

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,16 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import {rateLimit} from 'express-rate-limit'; 
 
 const app = express();
+
+// configure the rate Limiter
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes in milliseconds
+  max: 100, // Limit each IP to 100 requests per window (15 minutes)
+  message: 'Too many requests from this IP, please try again after 15 minutes'
+});
 
 app.use(
   cors({
@@ -14,6 +22,7 @@ app.use(
   })
 );
 
+app.use(limiter); 
 app.use(cookieParser());
 // use public folder to store the image uploaded by the user locally
 
