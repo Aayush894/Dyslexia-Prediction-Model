e319fc4bb961fc1e508a1c4eba6f88a3c37ca86d
Aayush Kumar Shrivastava <aayushs894@gmail.com>
2024-05-25 18:21:40 +0530

    some changes

--- backend/controllers/prediction.controller.js
+++ backend/controllers/prediction.controller.js
@@ -1,4 +1,5 @@
 import fetch from "node-fetch";
+import axios from 'axios';
 
 const imagePrediction = async (req, res) => {
   try {
@@ -10,22 +11,22 @@ const imagePrediction = async (req, res) => {
       throw new Error("IMAGE_URL is not defined in environment variables");
     }
 
-    const response = await fetch(resultUrl, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ text }),
+    const response = await axios.post(resultUrl, { text }, {
+      headers: { "Content-Type": "application/json" }
     });
 
-    console.log(response) ; 
+    console.log("Response status:", response.status);
+    console.log("Response data:", response.data);
 
-    if (!response.ok) {
-      throw new Error("Failed to fetch");
-    }
+    // if (response.ok === false){
+    //   throw new Error(`HTTP error! status: ${response.status}`);
+    // }
 
-    const data = await response.json(); // Parse response body as JSON
+    // const data = await (); // Parse response body as JSON
+    const data = response.data ; 
 
     console.log(data) ; 
-    
+
     if (data && data.result) {
       res.status(200).json({ result: data.result });
     } else {
