fdeb77d4136e27097098f8990be196ba618e6a58
Aayush Kumar Shrivastava <aayushs894@gmail.com>
2024-11-17 21:26:07 +0530

    changes for vercel deployemnt

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@ import path from "path";
 import dotenv from "dotenv";
 import connectDB from "./db/db.js";
 import { app } from "./app.js";
-import express from "express";
 
 dotenv.config();
 
@@ -13,12 +12,6 @@ dotenv.config({
 });
 
 const PORT = process.env.PORT || 5000;
-
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-app.get("*",(req, res) => {
-  res.sendFile(path.join(__dirname, "/frontend/dist/index.html"));
-});
 
 app.listen(PORT, () => {
   connectDB();
