c6a035b7349237755a86fb66f0048aa280036225
Aayush Kumar Shrivastava <114735569+Aayush894@users.noreply.github.com>
2024-11-17 09:30:40 +0530

    Update vite.config.js
    changes in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target:`${import.meta.env.VITE_BACKEND_URL}`,
       },
     },
   },
