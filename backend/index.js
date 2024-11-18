418ed997740d05cf5f89f167feed4aeb440bef0d
Aayush Kumar Shrivastava <aayushs894@gmail.com>
2024-11-18 22:08:51 +0530

    Railway Deploy

--- backend/index.js
+++ backend/index.js
@@ -2,23 +2,14 @@ import path from "path";
 import dotenv from "dotenv";
 import connectDB from "./db/db.js";
 import { app } from "./app.js";
-import express from "express";
 
 dotenv.config();
 
-const __dirname = path.resolve();
-
 dotenv.config({
-  path: "./.env",
+  path: "../.env",
 });
 
 const PORT = process.env.PORT || 5000;
-
-// app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-// app.get("*",(req, res) => {
-//   res.sendFile(path.join(__dirname, "/frontend/dist/index.html"));
-// });
 
 app.listen(PORT, () => {
   connectDB();
